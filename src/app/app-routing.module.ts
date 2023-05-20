c52a1a9119a37e5ca0a328676c8b1fb6acd60754
michael-otieno <otieno.oduor.michael>
2023-05-21 01:14:10 +0300

    :hammer: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/common/home/home.component';
+import { VettingComponent } from './components/work/vetting/vetting.component';
+import { BookComponent } from './components/service/book/book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'vetting', component:VettingComponent},
+  {path:'book', component:BookComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
