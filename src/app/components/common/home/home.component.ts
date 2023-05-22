1185e060acd4f3e31abfdf28a5af6a085002d91f
michael-otieno <otieno.oduor.michael>
2023-05-22 16:04:30 +0300

    add methods, to be implemented

--- src/app/components/common/home/home.component.ts
+++ src/app/components/common/home/home.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+signUp() {
+throw new Error('Method not implemented.');
+}
  testimonials = [
   {
     id:1,
