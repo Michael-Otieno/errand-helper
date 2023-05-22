1a29543d16abdca752d5ac8852d071d48cc6dd7a
michael-otieno <otieno.oduor.michael>
2023-05-22 14:48:02 +0300

    add signup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/common/home/home.component';
 import { VettingComponent } from './components/work/vetting/vetting.component';
 import { BookComponent } from './components/service/book/book.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
+  {path:'signup', component:SignupComponent},
   {path:'vetting', component:VettingComponent},
   {path:'book', component:BookComponent}
 
