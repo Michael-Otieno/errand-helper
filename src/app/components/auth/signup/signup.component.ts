e75808b94b32d10d8b58daf49a824f04d9fdf61b
michael-otieno <otieno.oduor.michael>
2023-05-25 12:28:34 +0300

    add form vallidation

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
-  signUp(){
-    console.log('sign up')
+
+  signupForm = this.fb.group({
+    firstName: ['',Validators.required],
+    lastName: ['',Validators.required],
+    email: ['',[Validators.required,Validators.email]],
+    terms: ['',Validators.required],
+    password: ['',Validators.required],
+    confirmPassword: ['',Validators.required],
+  });
+  constructor(private fb: FormBuilder) {}
+
+  signUp() {
+    throw new Error('Method not implemented.');
+  }
+  onSubmit() {
+    console.log(this.signupForm.value);
   }
 }
