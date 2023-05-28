56b837b4e31619dfe1203e2581f308c73c195121
michael-otieno <otieno.oduor.michael>
2023-05-28 07:32:20 +0300

    feat: :sparkles: add logout function and menutype

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 @Component({
@@ -8,17 +8,23 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class NavbarComponent implements OnInit {
   matMenu: any;
-  menuType: string = 'default';
+
+  @Input()
+  menuType!: string;
 
   constructor(private route: Router, public authService: AuthService) {}
 
   ngOnInit(): void {
     this.route.events.subscribe((val: any) => {
-      if (localStorage.getItem('user')) {
-        this.menuType = 'authenticated';
-      } else {
+      if (localStorage.getItem('user') === null) {
         this.menuType = 'default';
+      } else {
+        this.menuType = 'authenticated';
       }
     });
+  }
+
+  logout(){
+    this.authService.SignOut();
   }
 }
