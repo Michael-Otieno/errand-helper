4271c6f771e4e4c7ddd4c4df52ba4a760f7c3d4d
michael-otieno <otieno.oduor.michael>
2023-05-28 07:29:26 +0300

    refactor: :fire: remove comments

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -111,17 +111,8 @@ export class AuthService {
   SignOut() {
     return this.afa.signOut().then(() => {
       localStorage.removeItem('user');
-      this.route.navigateByUrl('/login');
+      this.route.navigateByUrl('/');
     });
   }
-  // constructor(private afA:AngularFireAuth,private afDB: AngularFireDatabase) { }
-  // loginWithGoogle(){
-  //   return this.afA.signInWithPopup(new GoogleAuthProvider());
-  // }
-  // registerWithDetails(user:{firstName: string, lastName: string, email: string, terms: string, password: string, confirmPassword: string}){
-  //   return this.afA.createUserWithEmailAndPassword(user.email,user.password)
-  // }
-  // loginWithEmailAndPassword(user:{email:string,password:string}){
-  //   return this.afA.signInWithEmailAndPassword(user.email,user.password)
-  // }
+
 }
