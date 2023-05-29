31fcbeeeeb26cc3bed9e16e47ade12dc9bf3689f
michael-otieno <otieno.oduor.michael>
2023-05-29 09:25:02 +0300

    feat: :art: add notifications array

--- src/app/components/work/notification/notification.component.ts
+++ src/app/components/work/notification/notification.component.ts
@@ -3,11 +3,52 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.scss']
+  styleUrls: ['./notification.component.scss'],
 })
 export class NotificationComponent {
-submit() {
-throw new Error('Method not implemented.');
-}
+  notifications = [
+    {
+      id: 1,
+      email: 'Errandhelper.com',
+      time: '3 days ago',
+      message:
+        'My JobMag is your favourite job website in Kenya. We make job search simple as we provide you with all jobs in Kenya in an easy and timely manner. Click on each specialization or job field to view jobs in each.',
+    },
+    {
+      id: 2,
+      email: 'Errandhelper.com',
+      time: '3 days ago',
+      message:
+        'My JobMag is your favourite job website in Kenya. We make job search simple as we provide you with all jobs in Kenya in an easy and timely manner. Click on each specialization or job field to view jobs in each.',
+    },
+    {
+      id: 3,
+      email: 'Errandhelper.com',
+      time: '3 days ago',
+      message:
+        'My JobMag is your favourite job website in Kenya. We make job search simple as we provide you with all jobs in Kenya in an easy and timely manner. Click on each specialization or job field to view jobs in each.',
+    },
+  ];
 
+  errands = [
+    {
+      id: 1,
+      email: 'Errandhelper.com',
+      time: '3 days ago',
+      review:
+        'My JobMag is your favourite job website in Kenya. We make job search simple as we provide you with all jobs in Kenya in an easy and timely manner. Click on each specialization or job field to view jobs in each.',
+    },
+    {
+      id: 2,
+      email: 'Errandhelper.com',
+      time: '3 days ago',
+      review:
+        'My JobMag is your favourite job website in Kenya. We make job search simple as we provide you with all jobs in Kenya in an easy and timely manner. Click on each specialization or job field to view jobs in each.',
+    },
+
+  ];
+
+  submit() {
+    throw new Error('Method not implemented.');
+  }
 }
