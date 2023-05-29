04d4a856f6ad13980bf64677db0551051628bb63
michael-otieno <otieno.oduor.michael>
2023-05-29 16:51:24 +0300

    feat: :sparkles: ahow logged in user

--- src/app/components/work/profile/profile.component.ts
+++ src/app/components/work/profile/profile.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/interfaces/user.interface';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
-submit() {
-throw new Error('Method not implemented.');
-}
+  user: User | null = null;
 
+  constructor(private authService: AuthService){
+    this.authService.currentUser$.subscribe((user) => {
+      this.user = user;
+
+      console.log(user?.firstName)
+    })
+  }
+
+  submit() {
+    throw new Error('Method not implemented.');
+  }
 }
